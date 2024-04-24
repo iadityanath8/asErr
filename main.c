0684ab4bdae59f8fcb03712df4388f08b6ca5040
iadityanath8 <iadityanath8@gmail.com>
2024-04-25 00:58:11 +0530

    Done experimenting asm

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#define TEST_ON 1
+#define TEST_OFF 0
+
 
 /*
- *
+ *                        PROPOSITION AND NOTES 
  *
  *  
  *  PROPOSITION -> hide the stupid conplexity of asm volatile kind of 
@@ -10,10 +13,19 @@
  *  --> first for only basic values which are copy semantics 
  *  --> then we will go crazy on making macros to have pointer value 
  *  --> semantics 
+ *  
+ *  PROPOSITION -> Now Only for GNU Linux x86_64 
+ *  --> Design Decision == Error Codes Like wrapped into enums (Kinda errno but returning stuff)
+ *  --> Design Decision == Return NULL like go to have an error checking thing 
  *
- *
+ *  NOTE -> 1->  () means dereference of a intermediate or a register both
+ *          2->  mov for regular 32 bit moving of value in a register 
+ *          3->  movq for moving quadwords or 64 bit value in a 64 bit register
+ *          4-> Withing the usage of a pointer we can also use any mov or movq but not in case of normal values 
  *
  */
+
+
 
 #if 0
 int main() {
@@ -45,13 +57,81 @@ int ret_val(){
   asm volatile (
     "mov %0, %%ebx\n\t"
     :
-    :"r" (value)
+    : "r" (value)
     );
 
   return 20;
 }
 
-#if 1
+
+typedef struct{
+  int x,y;
+}Point;
+
+#if TEST_ON
+
+int main(){
+  Point a = {1,2};
+  Point* ptr = &a;
+  
+  Point* new_ptr = {0};
+
+  asm volatile(
+    "movq %0, %%rax\n\t"
+    : "=r" (new_ptr)
+    :"r" (ptr)
+  );
+  
+  printf("%d\n",new_ptr->x);
+}
+
+#endif
+
+
+
+#if TEST_OFF
+
+int main(){
+  int value = 12;
+  int* ptr = NULL;
+  
+  int* pppre = (int*)0x123456;// = {0};
+  asm volatile(
+    "movq %0, %%rbx\n\t"
+    : 
+    : "r" (ptr)
+  );
+  
+  asm volatile(
+    "movq %%rbx, %0"
+    : "=r" (pppre)
+  );
+  
+  printf("%ls\n",pppre);
+  return 0;
+}
+
+#endif
+
+#if TEST_OFF
+int main() {
+    int value = 10;
+    int *ptr = &value;
+    int result;
+
+    asm volatile (
+        "movl (%1), %0\n\t"  // Move the value pointed to by ptr into result
+        : "=r" (result)  // Output operand
+        : "r" (ptr)  // Input operand
+    );
+
+    printf("Dereferenced value: %d\n", result);
+
+    return 0;
+}
+#endif
+
+#if TEST_OFF
 int main() {
   int x = 10;
   int y = 0;
@@ -63,7 +143,7 @@ int main() {
 //  );
   
   y = ret_val();
-  
+ 
   asm volatile (
     "mov %%ebx, %0\n\t"
     : "=r" (x)
